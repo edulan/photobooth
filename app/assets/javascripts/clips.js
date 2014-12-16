a2b0a6c233abbde86872851aec2bb90250942af8
Eduardo Lanchares <edulanchi@gmail.com>
2014-12-16 13:18:52 +0100

    Refactor clips listing to a Marionette app

--- app/assets/javascripts/clips.js
+++ app/assets/javascripts/clips.js
@@ -1,122 +1,45 @@
 $(function() {
-    if (window.location.pathname !== '/clips/new') {
-        return;
-    }
+    var PhotoBooth = new Marionette.Application();
 
-    var $video = $("#video").hide(),
-        $message = $("#message").hide(),
-        $flash = $(".flash").hide(),
-        $clip = $(".clip-row"),
-        localMediaStream;
+    PhotoBooth.Clip = Backbone.Model.extend();
 
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
-
-    function successCallback(stream) {
-        var video = $video.get(0);
-
-        localMediaStream = stream;
-
-        $video.show();
-        $message.hide();
-
-        if (video.mozSrcObject !== undefined) {
-            video.mozSrcObject = stream;
-        } else {
-            video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-        }
-
-        video.play();
-    }
-
-    function errorCallback(error) {
-        console.error('An error occurred: [' + error.name + '] ' + error.message);
-
-        if (error.name === 'PermissionDeniedError') {
-            $message.text('Camera access has been denied :(');
-        }
-
-        $video.hide();
-        $message.show();
-    }
-
-    function startVideo() {
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({ video: true }, successCallback, errorCallback);
-        } else {
-            $message.text("Sorry, your browser does not support the HTML5 Stream API").show();
-            // Display a friendly "sorry" message to the user.
-        }
-    }
-
-    function stopVideo (argument) {
-        var video = $video.get(0);
-
-        video.pause();
-        video.src = '';
-
-        localMediaStream.stop();
-    }
-
-    function takeSnapshot () {
-        var dimensions = { width: 224, height: 140 },
-            $canvas = $("<canvas>")
-                .addClass("snapshot")
-                .attr(dimensions)
-                .appendTo($clip);
-
-        context = $canvas.get(0).getContext("2d");
-        context.drawImage(video, 0, 0, dimensions.width, dimensions.height);
-    }
-
-    function postClip () {
-        var canvas = $(".snapshot").get(0),
-            data = new FormData();
-
-        canvas.toBlob(function(blob) {
-            data.append("clip[frame]", blob, "snapshot.png");
-
-            $.ajax({
-                url: '/clips',
-                data: data,
-                dataType: 'json',
-                processData: false,
-                contentType: false,
-                // mimeType: 'multipart/form-data',
-                type: 'POST',
-                success: function () {
-                    $flash.find(".flash-message").text("Your clip has been saved :)");
-                    $flash.show();
-                    stopVideo();
-                },
-                error: function() {
-                    $flash.find(".flash-message").text("Ups and error ocurred and we couldn't save your clip. Want to try again?");
-                    $flash.show();
-                }
-            });
-        }, "image/png");
-    }
-
-    $("#snap").on("click", function(event) {
-        var remainingSnapshots = 4,
-            snapInterval = 1000;
-
-        function takeNext () {
-            takeSnapshot();
-            remainingSnapshots--;
-
-            if (remainingSnapshots === 0) {
-                postClip();
-                return;
-            }
-
-            setTimeout(takeNext, snapInterval);
-        }
-
-        setTimeout(takeNext, snapInterval);
-
-        event.preventDefault();
+    PhotoBooth.Clips = Backbone.Collection.extend({
+        url: "/clips",
+        model: PhotoBooth.Clip
     });
 
-    startVideo();
+    PhotoBooth.ClipItemView = Marionette.ItemView.extend({
+        template: _.template([
+            '<div class="row">',
+            '<div class="clip-image-wrapper"><img class="clip-image" src="<%= frame.thumb_url %>" alt=""></div>',
+            '<div class="clip-image-wrapper"><img class="clip-image" src="<%= frame.thumb_url %>" alt=""></div>',
+            '<div class="clip-image-wrapper"><img class="clip-image" src="<%= frame.thumb_url %>" alt=""></div>',
+            '<div class="clip-image-wrapper"><img class="clip-image" src="<%= frame.thumb_url %>" alt=""></div>',
+            '</div>'
+        ].join("\n")),
+        className: "clip-group"
+    });
+
+    PhotoBooth.ClipsView = Marionette.CollectionView.extend({
+        className: "clip-list",
+        childView: PhotoBooth.ClipItemView
+    });
+
+    PhotoBooth.addRegions({
+        mainRegion: ".container",
+        clipsRegion: ".row-clip"
+    });
+
+    PhotoBooth.addInitializer(function() {
+        var collection = new PhotoBooth.Clips();
+        var view = new PhotoBooth.ClipsView({
+            collection: collection
+        });
+
+        PhotoBooth.clipsRegion.show(view);
+
+        collection.fetch();
+    });
+
+    PhotoBooth.start();
 });
--- app/views/clips/index.json.jbuilder
+++ app/views/clips/index.json.jbuilder
@@ -1,4 +1,8 @@
 json.array!(@clips) do |clip|
   json.extract! clip, :id
+  json.frame do
+    json.medium_url clip.frame.url(:medium)
+    json.thumb_url clip.frame.url(:thumb)
+  end
   json.url clip_url(clip, format: :json)
 end
