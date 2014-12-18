0176e24b555d3b6c42c5b311608ec94d2f7ac27a
Eduardo Lanchares <edulanchi@gmail.com>
2014-12-18 15:20:54 +0100

    Configure S3 for reduced redundancy storage

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,6 +3,8 @@ Paperclip::Attachment.default_options[:use_timestamp] = false
 Paperclip::Attachment.default_options[:storage] = :s3
 Paperclip::Attachment.default_options[:bucket] = ENV["S3_BUCKET"]
 Paperclip::Attachment.default_options[:s3_host_name] = "s3-eu-west-1.amazonaws.com"
+Paperclip::Attachment.default_options[:s3_protocol] = ""
+Paperclip::Attachment.default_options[:s3_storage_class] = :reduced_redundancy
 Paperclip::Attachment.default_options[:s3_credentials] = {
   access_key_id: ENV["S3_ACCESS_KEY_ID"],
   secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
