29bf86d079aff99d51834464ebd564745c3b07a9
Eduardo Lanchares <edulanchi@gmail.com>
2014-12-18 12:27:30 +0100

    Add more scenarios to listing clips feature

--- spec/features/listing_clips_spec.rb
+++ spec/features/listing_clips_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 feature "Listing clips", type: :feature, js: true do
+  context "with at least one clip" do
+    background do
+      create_list(:clip, 3, votes: 10)
+    end
 
-  background do
-    create_list(:clip, 3)
+    scenario "shows all created clips" do
+      visit "#clips"
+
+      expect(page).to have_css(".clip-group", count: 3)
+    end
+
+    scenario "shows clip votes" do
+      visit "#clips"
+
+      expect(page).to have_content("10", count: 3)
+    end
   end
 
-  scenario "shows all created clips" do
-    visit "#clips"
+  context "with no clips" do
+    scenario "shows no clips found message" do
+      visit "#clips"
 
-    expect(page).to have_css(".clip-group", count: 3)
+      expect(page).to have_content("No clips found")
+    end
   end
 end
