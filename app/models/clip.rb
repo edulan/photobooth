eb02511193abd51be5e46094b4a8556f8e43cfd7
Eduardo Lanchares <eduardo.lanchares@redbooth.com>
2015-02-13 16:48:59 +0100

    :trollface:

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,28 +1,28 @@
-class Clip < ActiveRecord::Base
-  (1..4).each do |i|
-    snapshot_sym = "snapshot#{i}".to_sym
+  class Clip < ActiveRecord::Base
+    (1..4).each do |i|
+      snapshot_sym = "snapshot#{i}".to_sym
 
-    has_attached_file snapshot_sym,
-      path: "uploads/:id_partition/:hash.:extension",
-      url: "uploads/:id_partition/:hash.:extension",
-      default_url: "assets/:style/missing.png",
-      size: { in: 0..250.kilobytes },
-      styles: { medium: ["300x300>", :jpg], thumb: ["100x100>", :jpg] },
-      preserve_files: true,
-      hash_secret: Rails.application.secrets.secret_key_base
+      has_attached_file snapshot_sym,
+        path: "uploads/:id_partition/:hash.:extension",
+        url: "uploads/:id_partition/:hash.:extension",
+        default_url: "assets/:style/missing.png",
+        size: { in: 0..250.kilobytes },
+        styles: { medium: ["300x300>", :jpg], thumb: ["100x100>", :jpg] },
+        preserve_files: true,
+        hash_secret: Rails.application.secrets.secret_key_base
 
-    validates_attachment_content_type snapshot_sym,
-      content_type: /\Aimage\/.*\Z/
+      validates_attachment_content_type snapshot_sym,
+        content_type: /\Aimage\/.*\Z/
+    end
+
+    (1..4).each do |i|
+      snapshot_sym = "snapshot#{i}".to_sym
+
+      process_in_background snapshot_sym,
+        processing_image_url: "assets/:style/processing.png"
+    end
+
+    def snapshots
+      (1..4).map { |i| self.send("snapshot#{i}".to_sym) }
+    end
   end
-
-  (1..4).each do |i|
-    snapshot_sym = "snapshot#{i}".to_sym
-
-    process_in_background snapshot_sym,
-      processing_image_url: "assets/:style/processing.png"
-  end
-
-  def snapshots
-    (1..4).map { |i| self.send("snapshot#{i}".to_sym) }
-  end
-end
