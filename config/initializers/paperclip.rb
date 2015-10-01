06c8d67014327d1ba8d3744a18a1b6a68af440f6
Eduardo Lanchares <eduardo@elanchares.com>
2015-10-01 22:57:15 +0200

    Move paperclip configuration to initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,10 +1,18 @@
-Paperclip::Attachment.default_options[:use_timestamp] = false
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:bucket] = ENV["S3_BUCKET"]
-Paperclip::Attachment.default_options[:s3_host_name] = "s3-eu-west-1.amazonaws.com"
-Paperclip::Attachment.default_options[:s3_protocol] = ""
-Paperclip::Attachment.default_options[:s3_storage_class] = :reduced_redundancy
-Paperclip::Attachment.default_options[:s3_credentials] = {
-  access_key_id: ENV["S3_ACCESS_KEY_ID"],
-  secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
-}
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:path] = "uploads/:id_partition/:hash.:extension"
+  Paperclip::Attachment.default_options[:url] = "uploads/:id_partition/:hash.:extension"
+  Paperclip::Attachment.default_options[:use_timestamp] = false
+  Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:bucket] = ENV["S3_BUCKET"]
+  Paperclip::Attachment.default_options[:s3_host_name] = "s3-eu-west-1.amazonaws.com"
+  Paperclip::Attachment.default_options[:s3_protocol] = ""
+  Paperclip::Attachment.default_options[:s3_storage_class] = :reduced_redundancy
+  Paperclip::Attachment.default_options[:s3_credentials] = {
+    access_key_id: ENV["S3_ACCESS_KEY_ID"],
+    secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
+  }
+else
+  Paperclip::Attachment.default_options[:path] = ":rails_root/public/uploads/:id_partition/:hash.:extension"
+  Paperclip::Attachment.default_options[:url] = "uploads/:id_partition/:hash.:extension"
+  Paperclip::Attachment.default_options[:use_timestamp] = false
+end
