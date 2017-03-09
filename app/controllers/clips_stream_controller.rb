435119286700ceec830000201dd119b408c72dd8
Eduardo Lanchares <eduardo@elanchares.com>
2017-03-09 09:40:11 +0100

    Remove clips live streaming :(

--- app/controllers/clips_stream_controller.rb
+++ app/controllers/clips_stream_controller.rb
@@ -1,7 +1,3 @@
-require 'thread'
-require 'heartbeat'
-require 'clips_subscriber'
-
 class ClipsStreamController < ActionController::Base
   include ActionController::Live
 
@@ -10,35 +6,6 @@ class ClipsStreamController < ActionController::Base
     return render head: :not_found unless booth
 
     response.headers['Content-Type'] = 'text/event-stream'
-
-    event_sources = []
-    queue = Queue.new
-
-    Thread.abort_on_exception = true
-
-    event_sources << Thread.new do
-      Heartbeat.on_beat(frequency: 15) do |notification|
-        Rails.logger.info "[ClipsStreamController] Ping..."
-        queue.push(notification)
-      end
-    end
-    event_sources << Thread.new do
-      # TODO: Extract channel name to a class???
-      ClipsSubscriber.new(booth.id).on_save do |notification|
-        Rails.logger.info "[ClipsStreamController] Notified #{notification[:data][:id]}"
-        queue.push(notification)
-      end
-    end
-
-    sse = SSE.new(response.stream)
-    while (notification = queue.pop)
-      sse.write(notification[:data], event: notification[:event])
-    end
-  rescue IOError => error
-    Rails.logger.info "[ClipsStreamController] #{error}, disconnecting..."
-    event_sources.each(&:exit)
-  ensure
-    Rails.logger.info "[ClipsStreamController] closing stream..."
-    sse.close
+    response.stream.close
   end
 end
